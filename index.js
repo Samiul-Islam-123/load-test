b56345a4fdbe917979bd0e21d1505ec9289306a2
MD Samiul Islam <isamiul099@gmail.com>
2026-01-30 23:04:57 +0530

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 5500 ;
+const PORT = process.env.PORT || 3000 ;
 
 app.get('/health', (req,res) => {
     res.send("Express server is running properly")
